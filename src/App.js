0c775204754f95450fd203e15cc62ab46c2396a2
Gusta <lgx.0097@gmail.com>
2020-03-17 20:57:53 -0300

    marking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className='app'> 
+    <div className='search'>
+      <input 
+        type='search' 
+        placeholder='Digite o nome do usuário no GitHub'/>
     </div>
-  );
-}
+
+    <div className='user-info'>
+      <img src='https://avatars1.githubusercontent.com/u/37967122?v=4'/>
+      <h1>
+        <a href='https://github.com/athekes'>Gustavo Castro</a>
+      </h1>
+      
+      <ul className='repos-info'>
+        <li> Repositórios: </li>
+        <li> Seguidores: </li>
+        <li> Seguindo: </li>
+      </ul>
+
+      <div className='actions'>
+        <button>Ver Repositórios</button>
+        <button>Ver Favoritos</button>
+      </div>
+
+      <div className="repos">
+        <h2>Repositórios</h2>
+        <ul>
+          <li><a href='#'>Nome do Repositório</a></li>
+        </ul>
+      </div>
+
+
+      <div className="starred">
+        <h2>Favoridos</h2>
+        <ul>
+          <li><a href='#'>Nome do Repositório</a></li>
+        </ul>
+      </div>
+
+     </div>
+  </div>
+)
 
 export default App;
