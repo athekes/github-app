059ac84956d2ca2bb88afcff6ddb84e2fde3ddd4
Gusta <lgx.0097@gmail.com>
2020-03-24 16:39:42 -0300

    separating into components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
 import React from 'react';
+import Search from './components/search'
+import UserInfo from './components/user-info'
+import Actions from './components/actions'
+import Repos from './components/repos'
 import './App.css';
 
 const App = () => (
   <div className='app'> 
-    <div className='search'>
-      <input 
-        type='search' 
-        placeholder='Digite o nome do usuário no GitHub'/>
-    </div>
 
-    <div className='user-info'>
-      <img src='https://avatars1.githubusercontent.com/u/37967122?v=4'/>
-      <h1>
-        <a href='https://github.com/athekes'>Gustavo Castro</a>
-      </h1>
-      
-      <ul className='repos-info'>
-        <li> Repositórios: </li>
-        <li> Seguidores: </li>
-        <li> Seguindo: </li>
-      </ul>
+    <Search/>
 
-      <div className='actions'>
-        <button>Ver Repositórios</button>
-        <button>Ver Favoritos</button>
-      </div>
+    <UserInfo/>
 
-      <div className="repos">
-        <h2>Repositórios</h2>
-        <ul>
-          <li><a href='#'>Nome do Repositório</a></li>
-        </ul>
-      </div>
+    <Actions/>
 
-
-      <div className="starred">
-        <h2>Favoridos</h2>
-        <ul>
-          <li><a href='#'>Nome do Repositório</a></li>
-        </ul>
-      </div>
-
-     </div>
+    <Repos 
+      className='repos' 
+      title='Repositórios'
+      repos={[{
+        name:'Nome do repositório',
+        link:'#'
+      }]}
+    />
+    <Repos 
+      className='starred' 
+      title='Favoritos'
+      repos={[{
+        name:'Nome do repositório',
+        link:'#'
+      }]}
+    />
+    
   </div>
 )
 
---
+++ src/components/user-info.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars1.githubusercontent.com/u/37967122?v=4' alt="fot"/>
+    <h1>
+      <a href='https://github.com/athekes'>Gustavo Castro</a>
+    </h1>
+    
+    <ul className='repos-info'>
+      <li> Repositórios: </li>
+      <li> Seguidores: </li>
+      <li> Seguindo: </li>
+    </ul>
+ </div>
+)
+
+export default UserInfo
